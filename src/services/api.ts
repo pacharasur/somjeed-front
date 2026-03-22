b56e3deaa0357dec0bf13e41ad51a312211ac130
pacharasur <94338617+pacharasur@users.noreply.github.com>
2026-03-23 02:48:41 +0700

    add dockerfile

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
-// const API_BASE_URL = 'http://localhost:8080'
-const API_BASE_URL = ''
+const API_BASE_URL = 'http://localhost:8080'
+// const API_BASE_URL = ''
 
 export type ChatRequest = {
   userId: string
